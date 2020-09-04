a67c89446fde52095503c14f25f5fae2d738b235
Youri <yboeckholtz@gmail.com>
2020-09-04 10:38:05 +0200

    fixed sizes of touchview

--- src/app/view/touch-view/product-list/product-list.component.ts
+++ src/app/view/touch-view/product-list/product-list.component.ts
@@ -9,7 +9,7 @@ import { Product } from '../../../models/product.model';
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [
-    new Product(1, 'Rotje', 5, 50, 'https://f.jwwb.nl/public/t/r/m/temp-saanvnuhjwtbpnttdyvs/g8zzbw/CF736AB9-18CC-47F7-9A0D-0BF3A4A9E1E3.jpeg'),
+    new Product(1, 'Rotje Deluxe Test', 5, 50, 'https://f.jwwb.nl/public/t/r/m/temp-saanvnuhjwtbpnttdyvs/g8zzbw/CF736AB9-18CC-47F7-9A0D-0BF3A4A9E1E3.jpeg'),
     new Product(1, 'Rotje', 5, 50, 'https://f.jwwb.nl/public/t/r/m/temp-saanvnuhjwtbpnttdyvs/g8zzbw/CF736AB9-18CC-47F7-9A0D-0BF3A4A9E1E3.jpeg'),
     new Product(1, 'Rotje', 5, 50, 'https://f.jwwb.nl/public/t/r/m/temp-saanvnuhjwtbpnttdyvs/g8zzbw/CF736AB9-18CC-47F7-9A0D-0BF3A4A9E1E3.jpeg'),
     new Product(1, 'Rotje', 5, 50, 'https://f.jwwb.nl/public/t/r/m/temp-saanvnuhjwtbpnttdyvs/g8zzbw/CF736AB9-18CC-47F7-9A0D-0BF3A4A9E1E3.jpeg'),
